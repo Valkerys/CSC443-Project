fce9c9243ddca410abc7c0d7e5fe798df61e347e
Carlos Espinoza <espinozc@augsburg.edu>
2019-10-17 13:10:50 -0500

    Adding framework and spring.

---
+++ src/main/java/CSC443Project/Csc443ProjectApplication.java
@@ -0,0 +1,13 @@
+package CSC443Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc443ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc443ProjectApplication.class, args);
+	}
+
+}
